63fb8c17d594f92dc9ae2f1f0679a51c606866d1
krabben16 <57534374+krabben16@users.noreply.github.com>
2022-06-08 23:21:18 +0900

    fix disable audits

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,8 +1,22 @@
+// https://github.com/GoogleChrome/lighthouse-ci/blob/v0.8.2/docs/getting-started.md#add-assertions
 module.exports = {
   ci: {
     // ...
     assert: {
       preset: 'lighthouse:recommended',
+      assertions: {
+        'apple-touch-icon': 'off',
+        'crawlable-anchors': 'off',
+        'csp-xss': 'off',
+        'html-has-lang': 'off',
+        'installable-manifest': 'off',
+        'link-name': 'off',
+        'maskable-icon': 'off',
+        'meta-description': 'off',
+        'service-worker': 'off',
+        'splash-screen': 'off',
+        'themed-omnibox': 'off',
+      },
     },
     // ...
   },
